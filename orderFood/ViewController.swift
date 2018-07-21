624ab07a0a7a8c8d0c16d289a36d967472a50e45
Anıl T <aniltaskiran@stu.aydin.edu.tr>
2018-07-21 21:40:19 +0300

    Initial Commit

---
+++ orderFood/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  orderFood
+//
+//  Created by Anıl T. on 21.07.2018.
+//  Copyright © 2018 anil. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
